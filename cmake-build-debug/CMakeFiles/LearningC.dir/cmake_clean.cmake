a7be649df73d9270343c61a245092a49b869dff5
cklaassen <klaassenconnor@gmail.com>
2020-09-24 15:55:11 -0400

    Initialize C environment

---
+++ cmake-build-debug/CMakeFiles/LearningC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearningC.dir/main.c.obj"
+  "LearningC.exe"
+  "LearningC.exe.manifest"
+  "LearningC.lib"
+  "LearningC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LearningC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
