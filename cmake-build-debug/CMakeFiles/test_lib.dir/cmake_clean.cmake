782049b69c72c809a1e01f6c835a35607b1e307b
cklaassen <klaassenconnor@gmail.com>
2020-09-24 20:51:12 -0400

    Figuring out linking files

---
+++ cmake-build-debug/CMakeFiles/test_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_lib.dir/test.c.obj"
+  "test_lib.lib"
+  "test_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test.c
@@ -0,0 +1,9 @@
+//
+// Created by klaas on 9/24/2020.
+//
+
+#include <stdio.h>
+
+void test() {
+    printf("TEST");
+}
